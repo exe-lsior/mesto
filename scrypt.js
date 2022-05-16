7b04bb640521c45f6eaee3a9ee97c84b2c6ff949
exe.lsior <exe.lsior@yandex.ru>
2022-05-17 00:47:34 +0300

    1st attempt

---
+++ scrypt.js
@@ -0,0 +1,51 @@
+//popup
+let popUp = document.getElementById('popup');
+let popUpOpen = document.getElementById('open_popup');
+let popUpClose = document.getElementById('close_popup');
+
+//savebutton
+let sendForm = document.getElementById('save-button');
+
+//elements
+let formElement = document.querySelector('.profile__info');
+let nameUser = document.querySelector('.profile__info-name');
+let jobUser  = document.querySelector('.profile__info-description');
+let nameInput = document.querySelector('.popup__container__form_name');
+let jobInput  = document.querySelector('.popup__container__form_description');
+
+
+
+function popUpActive () {
+    popUp.classList.add('active');
+    nameInput.value = nameUser.innerText;
+    jobInput.value = jobUser.innerText;
+};
+
+function popUpRemove () {
+    popUp.classList.remove('active');
+};
+
+function saveForm () {
+    nameInput.innerText = nameUser.value;
+    jobInput.innerText = jobUser.value;
+    formSubmitHandler(evt);
+}
+
+
+function formSubmitHandler (evt) {
+    evt.preventDefault(); 
+
+    nameUser.textContent = nameInput.value; 
+    jobUser.textContent = jobInput.value;
+
+    popUpRemove();
+};
+
+sendForm.addEventListener('click', formSubmitHandler);
+
+popUpOpen.addEventListener('click', popUpActive);
+
+popUpClose.addEventListener('click', popUpRemove);
+
+
+
